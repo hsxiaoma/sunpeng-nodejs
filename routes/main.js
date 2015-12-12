2fde9284086c9e8b1bb693b9b2e29fb87d01fc06
panyifei <981909093@qq.com>
2015-12-12 14:58:21 +0800

    chnage

--- routes/main.js
+++ routes/main.js
@@ -1,22 +1,11 @@
 var db = require("../connect").db;
 module.exports = function(req,res){
-    var menus = db.select().table('menu').exec(function(){
-        menus = menus._settledValue;
-
-        var users = db('user').where('id', 1).exec(function(){
-            users = users._settledValue;
-
-            var activitys = db.select().table('activity').exec(function(){
-                activitys = activitys._settledValue;
-
-                res.render("main", {
-                    menus:menus,
-                    users:users,
-                    activitys:activitys
-                });
-            });
-
+  db.select('*').from('plant').limit(8)
+  .then(function(resp) {
+        res.render("main", {
+          factorys:resp
         });
+  });
 
-    });
+
 }
